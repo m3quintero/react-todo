1fadcddf615ed9ab4ed7708f18803ea56d6ad776
Melissa Quintero <qmelissa310@gmail.com>
2024-11-05 11:56:17 -0800

    Added heading, To Do List variable, and changed text alignment to left

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+var todoList = [
+  {id: 1, title: "Complete the assignment"}, 
+  {id: 2, title: "Read for 10 minutes"},
+  {id: 3, title: "Clean my house"},
+  {id: 4, title: "Practice Code"}
+  ];
 
+function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>To Do List</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <ul>
+          {todoList.map(function (item) {
+            return <li key={item.id}>{item.title}</li>;
+          })}
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
